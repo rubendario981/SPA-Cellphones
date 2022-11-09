583873094d7ce87f014f3f58fb1c83a30924c01e
rubendario981 <rubendario981@hotmail.com>
2022-11-09 15:33:53 -0500

    feat(client/ app)
    Se agregan rutas para el renderizado de los componentes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import Home from "./views/Home/Products/Products";
 import { useDispatch } from "react-redux";
 import { getProducts } from "./redux/actions";
 import { Route, Routes } from "react-router-dom";
+import Products from "./views/Home/Products/Products";
+import ProductCard from "./views/Home/ProductCard/ProductCard";
+import Header from "./views/Home/Header/Header";
 
 function App() {
   const dispatch = useDispatch();
@@ -15,7 +18,12 @@ function App() {
   return (
     <React.Fragment>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path={"/"} element={<Header />} />
+        <Route exact path={"/home"} component={<Home />} />
+        <Route path={"productos"} component={Products}/>
+        <Route path={"detalleProducto/:id"} component={ProductCard}/>
+        <Route path={"filters"} component={Header}/>
+        <Route path={"createProduct"}/>
       </Routes>
     </React.Fragment>
   );
