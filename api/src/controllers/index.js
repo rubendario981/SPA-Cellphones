689c0e49b35f7bde63899f4f504c7c7d80b51e70
Snaider-Ar <snidergaona@gmail.com>
2022-11-09 12:22:32 -0500

    ruta name

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -2,11 +2,21 @@ const axios = require('axios');
 
 async function getAllProducts() {
   try {
-    let ApiInfo = await axios.get(
+    let ApiUrl = await axios.get(
       `https://api-celulares-27ad3-default-rtdb.firebaseio.com/.json`
     );
-    console.log(ApiInfo);
-    return ApiInfo.data;
+    let ApiInfo = await ApiUrl.data.map((el)=>{
+      return{
+        id: el.id,
+        image: el.image,
+        name: el.name,
+        brand: el.brand,
+        cpu: el.cpu,
+
+      }
+    })
+    console.log(ApiInfo)
+    return ApiInfo;
   } catch (error) {
     console.log(error);
   }
--- api/src/routes/routersControllers.js
+++ api/src/routes/routersControllers.js
@@ -1,13 +1,19 @@
-const { Router } = require('express');
-const { getAllProducts } = require('../controllers');
+const { Router } = require("express");
+const { getAllProducts } = require("../controllers");
 const router = Router();
 
-router.get('/', async (req, res) => {
-  try {
-    let respuesta = await getAllProducts();
-    res.json(respuesta);
-  } catch (error) {
-    res.json(error);
+router.get("/", async (req, res) => {
+  const name = req.query.name;
+  const respuesta = await getAllProducts();
+  if (name) {
+    let productName = await respuesta.filter((e) =>
+      e.name.toLowerCase().includes(name.toLowerCase())
+    );
+    productName.length
+      ? res.status(200).send(productName)
+      : res.status(404).send("Product not found");
+  }else {
+    res.status(200).send(respuesta)
   }
 });
 
