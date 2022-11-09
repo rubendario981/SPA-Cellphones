30d26cdfb9c0d51b58947898aaf3e5bda70d82e2
rubendario981 <rubendario981@hotmail.com>
2022-11-09 11:47:24 -0500

    feat(client/ router-dom)
    Se configuro el react router dom renderizando una ruta de prueba al home

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p className="bg-green-600 text-black">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <h1 className="text-3xl font-bold underline">
           Hello world!
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </h1>        
       </header>
     </div>
   );
