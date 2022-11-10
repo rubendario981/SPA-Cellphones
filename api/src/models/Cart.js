7d41869317630763f6be1f9c6b93f8a5a1ecdda7
sangaray <scgaraycochea@yahoo.com.ar>
2022-11-10 00:23:18 -0300

    se han modificado los models

--- api/src/models/Cart.js
+++ api/src/models/Cart.js
@@ -1,10 +1,10 @@
-const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo.
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define(
-    'cart',
+    "cart",
     {
       quantity: {
         type: DataTypes.INTEGER,
