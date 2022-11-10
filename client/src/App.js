02619ecf7c2f0f5ab924f15d98bed1fd007f5d00
Enzo Calvi <enzocalvi243@protonmail.com>
2022-11-09 23:35:20 -0300

    Routeo a la pagina principal

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getProducts } from "./redux/actions";
 import { Route, Routes } from "react-router-dom";
+
 import Products from "./views/Home/Products/Products";
 
 function App() {
