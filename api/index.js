1a34222db004f9fe1b038f9ef915d5a3f7118587
Sandra Garaycochea <scgaraycochea@yahoo.com.ar>
2022-11-10 22:34:43 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,8 +4,7 @@ const { conn } = require("./src/db.js");
 const { getAllProducts } = require("./src/controllers/index.js");
 
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, async () => {
+  server.listen(3001, () => {
     console.log("Servidor listening at 3001");
-    await getAllProducts();
   });
 });
