f084b9b6741185a086aec4f414285d5b07e1c4e9
fermin234 <fermin234@hotmail.com>
2022-11-10 21:24:25 -0300

    Modificaciones en los controllers

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -1,39 +1,64 @@
 const axios = require('axios');
 const { Cellphone } = require('../db.js');
+const { Op } = require('sequelize');
 
+//Trae todos los productos de la api y los vuelca a la base de datos
 async function getAllProducts() {
   try {
-    let ApiInfo = await axios.get(
-      `https://api-celulares-27ad3-default-rtdb.firebaseio.com/.json`
-    );
+    let products = await getProductsWithDB();
+    if (typeof products === 'string') {
+      products = await axios.get(
+        `https://api-celulares-27ad3-default-rtdb.firebaseio.com/.json`
+      );
+      products = products.data?.map((e) => ({
+        id: e.id,
+        brand: e.brand,
+        name: e.name,
+        image: e.image,
+        screen: e.screen,
+        internal_storage: e.internal_storage,
+        font_camera: e.font_camera,
+        rear_camera: e.rear_camera,
+        cpu: e.cpu,
+        ram: e.ram,
+        SO: e.SO,
+        battery: e.battery,
+        color: e.Color,
+        price: e.price,
+      }));
 
-    return ApiInfo.data;
+      await Cellphone.bulkCreate(products);
+    }
+
+    return products;
   } catch (error) {
     return error;
   }
 }
 
+//Trae un producto segun su id
 async function getProductById(id) {
-  let ApiInfo = await axios.get(
-    `https://api-celulares-27ad3-default-rtdb.firebaseio.com/.json`
-  );
+  try {
+    let product = await getProductsWithDB();
 
-  let product = ApiInfo.data.filter((e) => e.id === id);
+    product = product.filter((e) => e.id == id);
 
-  return product.length
-    ? product
-    : 'El ID no esta relacionado a ningun producto';
+    return product;
+
+    return product.length
+      ? product
+      : 'El ID no esta relacionado a ningun producto';
+  } catch (error) {}
 }
 
+//Trae todos los productos que en su nombre incluyan el name que se busca
 async function getProductByName(name) {
   try {
-    let ApiInfo = await axios.get(
-      `https://api-celulares-27ad3-default-rtdb.firebaseio.com/.json`
-    );
-
-    let product = ApiInfo.data.filter((e) =>
-      e.name.toLowerCase().includes(name.toLowerCase())
-    );
+    let product = await Cellphone.findAll({
+      where: {
+        name: { [Op.iLike]: `%${name}%` },
+      },
+    });
 
     return product.length ? product : 'Product not found';
   } catch (error) {
@@ -41,19 +66,23 @@ async function getProductByName(name) {
   }
 }
 
+//Crea un producto en la base de datos
 async function createProduct(product) {
   try {
     let [producto, creado] = await Cellphone.findOrCreate({
       where: {
+        brand: product.brand,
         name: product.name,
         image: product.image,
+        screen: product.screen,
+        internal_storage: product.internal_storage,
+        font_camera: 'asd',
+        rear_camera: product.rear_camera,
         cpu: product.cpu,
         ram: product.ram,
-        screen: product.screen,
+        SO: product.SO,
+        battery: product.battery,
         price: product.price,
-        front_camera: product.front_camera,
-        rear_camera: product.rear_camera,
-        internal_storage: product.internal_storage,
       },
       defaults: {
         product,
@@ -61,6 +90,19 @@ async function createProduct(product) {
     });
 
     return creado ? 'Se creo' : 'Ya existe ese celular';
+  } catch (error) {
+    return error;
+  }
+}
+
+//Trae de la base de datos todos los productos
+async function getProductsWithDB() {
+  try {
+    let DBInfo = await Cellphone.findAll();
+    if (!DBInfo.length) {
+      return 'La base de datos se encuentra vacia.';
+    }
+    return DBInfo;
   } catch (error) {
     return error;
   }
