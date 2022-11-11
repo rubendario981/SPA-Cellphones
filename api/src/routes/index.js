5e39eb5f4a053e43165540911ddb65927c148bda
fermin234 <fermin234@hotmail.com>
2022-11-10 23:27:28 -0300

    Se creo ruta y controller para la creacion de un user

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const router = Router();
 const controllers = require('./routersControllers.js');
+const routersUser = require('./routersUser.js');
 
 router.use('/products', controllers);
+router.use('/user', routersUser);
 
 module.exports = router;
