280205026f3140e74d3db54170a7a878bfee63e8
rubendario981 <rubendario981@hotmail.com>
2022-11-10 20:47:11 -0500

    feat(client/ Formulario creacion de usuario)
    Se crea componente para el registro de usuario, aun no esta terminada la funcionalidad para registrar usuario en la base de datos ni los cambios en redux

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import { getProducts } from "./redux/actions";
 import { Route, Routes } from "react-router-dom";
 import Products from "./views/Home/Products/Products";
 import Detail from "./views/Detail/DetailPhones";
+import FormProduct from "./views/FormProduct/FormProduct"
+import FormUser from "./views/FormUser/FormUser";
 
 export default function App() {
   const dispatch = useDispatch();
@@ -18,6 +20,8 @@ export default function App() {
       <Routes>
         <Route exact path={"/"} element={<Products />} />
         <Route exact path={"/product/:id"} element={<Detail />} />
+        <Route exact path={"/create"} element={<FormProduct />} />
+        <Route exact path={"/register"} element={<FormUser />} />
       </Routes>
     </React.Fragment>
   );
