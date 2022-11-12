10a31f8bf96cd07159c5c677ae651bbb35725adf
rubendario981 <rubendario981@hotmail.com>
2022-11-11 20:00:27 -0500

    feat(api/ controllers)
    Se cargan en la base de datos las marcas y sistemas operativos en la base de datos

--- api/src/db.js
+++ api/src/db.js
@@ -39,28 +39,19 @@ const { Cellphone, Bill, Brand, Os, Cart, Users } = sequelize.models;
 
 // Aca vendrian las relaciones
 Users.hasMany(Bill, {   foreignKey: "idUser" });
-Bill.belongsTo(Users); // ok
+Bill.belongsTo(Users);
 
 Users.hasOne(Cart, { foreignKey: "idUser"});
-Cart.belongsTo(Users); // ok
+Cart.belongsTo(Users);
 
-Bill.belongsToMany(Cellphone, { through: "CellphoneUser"});
-Cellphone.belongsToMany(Bill, { through: "CellphoneUser"});
+Bill.belongsToMany(Cellphone, { through: "CellphoneBill"});
+Cellphone.belongsToMany(Bill, { through: "CellphoneBill"});
 
 Os.hasMany(Cellphone, { foreignKey: "idOs"});
 Cellphone.belongsTo(Os);
 
 Brand.hasMany(Cellphone);
 Cellphone.belongsTo(Brand);
-
-// Users.hasMany(Cellphone, { foreignKey: "idUser" });
-// Cellphone.belongsTo(Users); // xxxx habia que establecer relacion muchos a muchos
-
-// Cellphone.hasOne(Brand, { foreignKey: "idCellphone" });
-// Brand.belongsTo(Cellphone); // xxx relacion uno a uno xxx
-
-// Brand.hasOne(Os, {   foreignKey: "idBrand" });
-// Os.belongsTo(Brand); // xxx relacion uno a uno xxx
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
